70d1f142bf6eb7a0ad39bc5da7aa0642b555f4e5
Yxa2111 <yxa2013@outlook.com>
2025-03-08 14:35:35 +0000

    add source han font

--- manga_translator/rendering/text_render.py
+++ manga_translator/rendering/text_render.py
@@ -150,6 +150,7 @@ def add_color(bw_char_map, color, stroke_char_map, stroke_color):
     return bg#, alpha_char_map
 
 FALLBACK_FONTS = [
+    os.path.join(BASE_PATH, 'fonts/SourceHanSansHW-VF.ttf.ttc'),
     os.path.join(BASE_PATH, 'fonts/Arial-Unicode-Regular.ttf'),
     os.path.join(BASE_PATH, 'fonts/msyh.ttc'),
     os.path.join(BASE_PATH, 'fonts/msgothic.ttc'),
